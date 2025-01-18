73c09a5d9825d28cc1bc1f9f00c8365f208c21f9
ben wainaina <bwainainamwangi@gmail.com>
2025-01-18 03:08:07 +0300

    feat: add font icons and custom fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '31.angular_and_angular_material_how_to_add_custom_icons';
+  public exampleMaterialIcons: Array<{ iconName: string; iconLabel: string }> =
+    [
+      { iconName: 'play_arrow', iconLabel: 'Play' },
+      { iconName: 'speed', iconLabel: 'Default Speed' },
+      { iconName: 'skip_next', iconLabel: 'Play Next' },
+      { iconName: 'video_library', iconLabel: 'Video Library' },
+      { iconName: 'headset_mic', iconLabel: 'Music Icon' },
+    ];
+  public exampleCustomIcons: Array<{ iconName: string; iconLabel: string }> = [
+    { iconName: 'shared__logo_icon', iconLabel: 'Logo' },
+    { iconName: 'shared__custom_play_icon', iconLabel: 'Custom Play' },
+    { iconName: 'shared__speed_red', iconLabel: 'Red Meter' },
+    { iconName: 'shared__speed_orange', iconLabel: 'Orange Meter' },
+    { iconName: 'shared__speed_green', iconLabel: 'Green Meter' },
+  ];
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,43 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  inject,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MatIconRegistry } from '@angular/material/icon';
+import { APP_ICONS } from './app.icons';
+import { provideHttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
+/**
+ * HINT: This can be moved into a utilities function
+ */
+const initializeAppIconsUtility = () => {
+  const matIconRegistry = inject(MatIconRegistry);
+  const domSanitizer = inject(DomSanitizer);
+
+  APP_ICONS.forEach((appIcon) =>
+    matIconRegistry.addSvgIconLiteral(
+      appIcon.name,
+      domSanitizer.bypassSecurityTrustHtml(appIcon.svg)
+    )
+  );
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+
+    // APP INITIALIZERS
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppIconsUtility,
+      deps: [MatIconRegistry],
+    },
+  ],
 };
