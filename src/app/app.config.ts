91626db9e22067fcb7cbe28ec7e2a0a48e2a9051
ben wainaina <bwainainamwangi@gmail.com>
2025-01-22 09:13:15 +0300

    feat: a bit of cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,23 @@
 import {
   APP_INITIALIZER,
   ApplicationConfig,
-  inject,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { MatIconRegistry } from '@angular/material/icon';
-import { provideHttpClient } from '@angular/common/http';
+
 import { initializeAppIconsUtility } from './utilities/initializeAppIcons.utility';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
 
     // APP INITIALIZERS
     {
       provide: APP_INITIALIZER,
       useFactory: initializeAppIconsUtility,
-      deps: [MatIconRegistry],
     },
   ],
 };
